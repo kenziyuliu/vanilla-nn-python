508271aff73a3c4e0bdd6bcef6dc0c86dc46e692
Ken Liu <liuziyu0816@hotmail.com>
2018-04-20 16:58:54 +1000

    Slight changes and a test case

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def to_one_hot_label(labels):
+def to_one_hot_labels(labels):
     one_hot_labels = np.zeros((len(labels), labels.max() + 1))
     one_hot_labels[np.arange(len(labels)), labels] = 1
     return one_hot_labels
@@ -10,4 +10,6 @@ if __name__ == '__main__':
     """
     Test cases
     """
-    print(to_one_hot_label(np.arange(10)))
+    labels = np.random.randint(0, 10, (15))
+    print(labels)
+    print(to_one_hot_labels(labels))
