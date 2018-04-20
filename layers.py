e9d6f54f9652ffe64a23efc5534a81d77eab64de
kenziyuliu <kenziyuliu@users.noreply.github.com>
2018-04-20 20:55:03 +1000

    utils and layers and config (#2)
    * added config, layers (relu and leaky relu), and updated utils
    
    * added a bunch of functions

--- layers.py
+++ layers.py
@@ -3,19 +3,45 @@ import numpy as np
 def relu(x):
     return np.maximum(x, 0)
 
+
 def leaky_relu(x, a=0.3):
     if a <= 0 or a > 1:
         raise ValueError('LeakyReLU: alpha must be between 0 and 1')
     return np.maximum(x, a * x)
 
 
+def softmax(x):
+    '''
+    Numerically more stable implementation of
+    softmax function. Ref: http://cs231n.github.io/linear-classify/
+    '''
+    x -= np.max(x)  # maximum value is now 0, so exponentials wont break
+    probabilities = np.exp(x) / np.sum(np.exp(x))
+    return probabilities
+
+
+def cross_entropy_loss(y_pred, y_true):
+    return -np.log(y_pred[np.argmax(y_true)])   # -negative log probability of the right class
+
+
 if __name__ == '__main__':
     """
     Test cases
     """
+    print('relu and leaky relu:')
     values = np.random.rand(4,5) - .5
     print(values)
     print(relu(values))
     print(leaky_relu(values, .1))
 
+    print('Softmax:')
+    x = np.array([1,2,3,4])
+    print(x)
+    print(softmax(x))
+
+    print('Cross entropy loss:')
+    y_pred = np.array([1,2,3,4,5])
+    y_true = np.array([0,0,1,0,0])
+    print('y_pred: {}, y_true: {}'.format(y_pred, y_true))
+    print('loss: {}'.format(cross_entropy_loss(y_pred, y_true)))
 
--- utils.py
+++ utils.py
@@ -1,16 +1,63 @@
 import numpy as np
 import config
 
+
+def shuffle_together(a, b):
+    '''
+    Shuffles 2 arrays with same length together
+    '''
+    assert len(a) == len(b) # Make sure they are of same dim
+    random_state = np.random.get_state()
+    np.random.shuffle(a)
+    np.random.set_state(random_state)
+    np.random.shuffle(b)
+
+
 def to_one_hot_labels(labels):
+    '''
+    Convert labels to one hot representation
+    '''
     one_hot_labels = np.zeros((len(labels), config.NUM_CLASSES))
     one_hot_labels[np.arange(len(labels)), labels] = 1
     return one_hot_labels
+
+
+def mean_normalisation(data):
+    if len(data.shape) != 2:
+        raise ValueError('Missing dimension! (n, dim) required')
+    mean = np.mean(data, axis=0)
+    data_min = np.amin(data, axis=0)
+    data_max = np.amax(data, axis=0)
+    data = (data - mean) / (data_max - data_min)
+    return data
+
+
+def whitening(data):
+    if len(data.shape) != 2:
+        raise ValueError('Missing dimension! (n, dim) required')
+    mean = np.mean(data, axis=0)
+    var = np.var(data, axis=0)
+    data = (data - mean) / var
+    return data
 
 
 if __name__ == '__main__':
     """
     Test cases
     """
+    print('to one hot:')
     labels = np.random.randint(0, 10, (15))
     print(labels)
     print(to_one_hot_labels(labels))
+
+    print('mean norm:')
+    # data = np.arange(9).reshape(3, 3)
+    data = np.random.randint(0,10, (3,3))
+    print(data)
+    print(mean_normalisation(data))
+
+    print('data whitening:')
+    data = np.random.randint(0,10, (3,3))
+    print(data)
+    print(whitening(data))
+
