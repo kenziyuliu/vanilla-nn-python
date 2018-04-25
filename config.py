2ecbdfc95c3488fa58527d4e471971c4b58c485a
kenziyuliu <kenziyuliu@users.noreply.github.com>
2018-04-25 21:46:07 +1000

    Layers framework (#3)
    * added config, layers (relu and leaky relu), and updated utils
    
    * added a bunch of functions
    
    * Finished most layers; added example usage

--- config.py
+++ config.py
@@ -2,4 +2,35 @@
 This file stores constants and configurations
 '''
 
+from layers import *
+
+INPUT_DIM = 128
 NUM_CLASSES = 10
+
+# Example usage
+sample_model = [
+    FullyConnected(INPUT_DIM, 256, weight_initializer=None),
+    LeakyReLU(alpha=0.1),
+    FullyConnected(256, 128, weight_initializer=None),
+    LeakyReLU(),
+    FullyConnected(128, 64, weight_initializer=None),
+    ReLU(),
+    FullyConnected(64, 10, weight_initializer=None),
+    Softmax()
+]
+
+# Example
+data = np.random.randint(1, 100, size=128)
+for layer in sample_model:
+    data = layer.forward(data)
+
+y_true = np.zeros(10)
+y_true[1] = 1
+
+lossfunction = CrossEntropyLoss()
+loss = lossfunction.forward(data, y_true)
+gradient = lossfunction.backward()
+
+for layer in reversed(sample_model):
+    gradient = layer.backward(gradient)
+
